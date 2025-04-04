0a67af293eac7bdb65b59e76fca3ccd16bba2495
olazochavesaxel <achaveso@ucenfotec.ac.cr>
2025-04-03 22:35:22 -0600

    Log in actualizando datos de profile

--- DataAccess/DAOs/SqlDAO.cs
+++ DataAccess/DAOs/SqlDAO.cs
@@ -34,7 +34,7 @@ namespace DataAccess.DAOs
             
 
 
-            _connectionString = @"Data Source=DESKTOP-S248HQ3;Initial Catalog=Proyecto_2;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+            _connectionString = @"Data Source=.;Initial Catalog=Proyecto_2;Integrated Security=True;Trust Server Certificate=True";
 
         }
 
